5e2cf8ee26452d9bfab70678c3a1da61de5b0ba8
Danila Nikolaev <danila_nikolaev@epam.com>
2025-06-19 16:08:26 +0400

    make Add generic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dartungar/go-module-test
 
 go 1.24.2
+
+require golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 // My test package.
 package main
 
+import "golang.org/x/exp/constraints"
+
 func main() {
 
 }
 
+type Number interface {
+	constraints.Float | constraints.Integer
+}
+
 // Adds two integers together and returns the result.
-func Add(a int, b int) int {
+func Add[T Number](a T, b T) T {
 	return a + b
 }
